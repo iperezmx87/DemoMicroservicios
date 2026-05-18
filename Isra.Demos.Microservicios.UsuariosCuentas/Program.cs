c95ca71809ed44a0bdb2e8c0624deb061f48a730
ISRAEL PEREZ SAUCEDO <israel.saucedo@2d-group.com>
2026-05-18 17:23:04 -0600

    chore: cors en backend

--- Isra.Demos.Microservicios.UsuariosCuentas/Program.cs
+++ Isra.Demos.Microservicios.UsuariosCuentas/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Configuración de CORS para permitir al frontend conectarse
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontendReact", policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins("http://localhost:5173", "http://localhost:4200", "http://localhost:5116")
               .AllowAnyHeader()
